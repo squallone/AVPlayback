4168509517f170696f51d890f1e028650fb2c327
Abdiel <asoto@etsy.com>
2025-12-28 18:59:22 -0600

    Initial Commit

---
+++ AVPlayback/AVPlaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlaybackApp.swift
+//  AVPlayback
+//
+//  Created by Abdiel Soto on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AVPlaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVPlaybackTests/AVPlaybackTests.swift
@@ -0,0 +1,16 @@
+//
+//  AVPlaybackTests.swift
+//  AVPlaybackTests
+//
+//  Created by Abdiel Soto on 12/28/25.
+//
+
+import Testing
+
+struct AVPlaybackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
